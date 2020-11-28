9945c2551af5c84be5158b040722ba7e0143ae44
Srinidhi011 <74104028+Srinidhi011@users.noreply.github.com>
2020-11-28 12:49:31 +0530

    Create ks.py

---
+++ ks.py
@@ -0,0 +1,51 @@
+from Adafruit_IO import Client, Feed , Data
+x = "sri_nidhi"
+y = "aio_yfye68ZoUU2IPg2DbC2WgKrVqsU6"
+aio = Client(x,y)
+
+def send_value(value):
+    feed = aio.feeds('tgbot')
+    aio.send_data(feed.key,value)
+
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+import requests
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(update, bot):
+  update.message.reply_text("/Turnon : To turn on the light /Turnoff : To turn off the light")
+
+def Turnon(update, bot):
+  update.message.reply_text("Light turned on,value=1 sent to adafruit_io feed")
+  bot.send_photo( chat_id = update.message.chat_id,photo = 'https://i.dlpng.com/static/png/507790_preview.png')
+  send_value(1)
+
+def Turnoff(update, bot):
+  update.message.reply_text("Light turned off ,value=0 sent to adafruit_io feed")
+  bot.send_photo(chat_id = update.message.chat_id, photo='https://i.dlpng.com/static/png/7501809_preview.png')
+  send_value(0)
+
+def input_message(update, bot):
+  text=update.message.text.upper()
+  text=update.message.text
+  if text == 'Turnon':
+    send_value(1)
+    update.message.reply_text("Light turned on,value=1 sent to adafruit_io feed")
+    bot.send_photo( chat_id = update.message.chat_id,photo ='https://i.dlpng.com/static/png/507790_preview.png')
+  elif text == 'Turnoff':
+    send_value(0)
+    update.message.reply_text("Light turned off,value=0 sent to adafruit_io feed")
+    bot.send_photo( chat_id = update.message.chat_id, photo'https://i.dlpng.com/static/png/7501809_preview.png')
+
+u = Updater('1435528708:AAHvEFNSBpD91-RkGaHJBgYolZkW0E5mbhE')
+dp = u.dispatcher
+dp.add_handler(CommandHandler('start',start))
+dp.add_handler(CommandHandler('Turnoff',Turnoff))
+dp.add_handler(CommandHandler('Turnon',Turnon))
+dp.add_handler(MessageHandler(Filters.text & (~Filters.command),input_message))
+u.start_polling()
+u.idle()
